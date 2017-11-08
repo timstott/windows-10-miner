874575e84b4004bf26f134e52fbd3228336851b2
Timothy Stott <stott.timothy@gmail.com>
2017-11-08 23:14:01 +0000

    Execute update in subshell

--- boot.sh
+++ boot.sh
@@ -13,16 +13,17 @@ fi
 while true
 do
   git remote update origin
-  echo "Updated remote"
+  echo "Pulled delta"
 
   if [ "$(git status -u no | grep 'Your branch is up-to-date')" ]
   then
     echo "No updates"
   else
-    git reset --hard origin/master
-    echo "Updated local"
     shutdown.exe -r -f -t $RESTART_TIMEOUT
     echo "Scheduled shutdown"
+    # Execute update in other process because Windows cannot update currently opened file
+    sh -c 'sleep 3; git reset --hard origin/master; echo "Updated code"; sleep 3' &
+    exit 0
   fi
 
   echo "Sleep $SLEEP_INTERVAL seconds"
