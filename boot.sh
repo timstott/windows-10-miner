be0ca879a479be2cbfe6372a07aa56e5c0ef820a
Timothy Stott <stott.timothy@gmail.com>
2017-11-09 00:23:31 +0000

    Add log function with ISO8601 timestamp prefix

--- boot.sh
+++ boot.sh
@@ -3,30 +3,37 @@
 export RESTART_TIMEOUT=90
 export SLEEP_INTERVAL=120
 
+log()
+{
+  echo "[`date -u +'%Y-%m-%dT%H:%M:%SZ'`] - ${*}"
+}
+
+log "Started boot.sh"
+
 if (($RESTART_TIMEOUT > $SLEEP_INTERVAL))
 then
-  echo "SLEEP_INTERVAL must be greater than RESTART_TIMEOUT!"
+  log "SLEEP_INTERVAL must be greater than RESTART_TIMEOUT!"
   exit 1
 fi
 
-cmd.exe /C "cd claymore-v10-0 && start start_only_eth.bat"
+# cmd.exe /C "cd claymore-v10-0 && start start_only_eth.bat"
 
 while true
 do
-  git remote update origin
-  echo "Pulled delta"
+  log "$(git remote update origin)"
+  log "Pulled delta"
 
   if [ $(git rev-list --count master..origin/master) -eq 0 ]
   then
-    echo "No updates"
+    log "No updates"
   else
     shutdown.exe -r -f -t $RESTART_TIMEOUT
-    echo "Scheduled shutdown"
+    log "Scheduled shutdown"
     # Execute update in other process because Windows cannot update currently opened file
-    sh -c 'sleep 3; git reset --hard origin/master; echo "Updated code"; sleep 3' &
+    sh -c 'sleep 3; git reset --hard origin/master; echo "Updated code"; sleep 10' &
     exit 0
   fi
 
-  echo "Sleep $SLEEP_INTERVAL seconds"
+  log "Sleep $SLEEP_INTERVAL seconds"
   sleep $SLEEP_INTERVAL
 done
