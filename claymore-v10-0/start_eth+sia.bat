cdab37db15a2dc923cebe32dffb85defa3f552de
Timothy Stott <stott.timothy@gmail.com>
2017-12-09 11:13:27 +0000

    Configure wallet and pools

--- claymore-v10-0/start_eth+sia.bat
+++ claymore-v10-0/start_eth+sia.bat
@@ -4,4 +4,4 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-EthDcrMiner64.exe -epool eth-eu1.nanopool.org:9999 -ewal YOUR_WALLET/YOUR_WORKER/YOUR_EMAIL -epsw x -dpool stratum+tcp://sia-eu1.nanopool.org:7777 -dwal YOUR_SIA_WALLET/YOUR_WORKER/YOUR_EMAIL -dpsw x -dcoin sia -ftime 10
+EthDcrMiner64.exe -epool eth-asia1.nanopool.org:9999 -ewal 0x18EB9cc4d02e1855b4f1D2c8777Be64b234d4E4b.buster/mining-rigs-cooperative@googlegroups.com -epsw x -dcoin sia -dpool stratum+tcp://sia-asia1.nanopool.org:7777 -dwal e51d669a8d5df110469215fd0daf40535dccf5f563259223821c039abfde52c9bd4aa443f5f8/buster/mining-rigs-cooperative@googlegroups.com -dpsw x -dcri 12 -ftime 3 -tstop 85 -tt 65 -fanmin 75 -fanmax 100 -cclock 1130 -mclock 2250 -cvddc 850 -mvddc 850
